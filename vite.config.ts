d6789215dfacb8d2236512d044b0c05d6f0acb13
Kim Grinfeder <kimgrinfeder@gmail.com>
2026-02-24 11:50:08 -0500

    updated readme file.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/",  // 👈 updated for root domain
+  base: "/",  
   server: {
     host: "::",
     port: 8080,
